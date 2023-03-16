6dd10afff51265fca2ee5907884e1c2b74281b24
J Michalszeszen <124604121+jmichalszeszen@users.noreply.github.com>
2023-03-16 10:16:44 -0300

    Update __init__.py

--- esphome/components/voip/__init__.py
+++ esphome/components/voip/__init__.py
@@ -2,8 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-CONF_PORT_VOIP = 5060
-
 voip_component_ns = cg.esphome_ns.namespace('voip_component')
 VoipComponent = voip_component_ns.class_('VoipComponent', cg.Component)
 
@@ -12,6 +10,6 @@ CONFIG_SCHEMA = cv.Schema({
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_PORT_VOIP])
+    var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
 
