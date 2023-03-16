60dfa1d78d9724aac8e529ced0a2a5c19a690bd7
J Michalszeszen <124604121+jmichalszeszen@users.noreply.github.com>
2023-03-16 11:23:21 -0300

    Update __init__.py

--- esphome/components/voip/__init__.py
+++ esphome/components/voip/__init__.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_SIP_URL
 
 voip_component_ns = cg.esphome_ns.namespace('voip_component')
 VoipComponent = voip_component_ns.class_('VoipComponent', cg.Component)
