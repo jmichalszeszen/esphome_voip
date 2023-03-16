709065dc1c5f2295e7bc44e5e528f8c7f9e6afa6
J Michalszeszen <124604121+jmichalszeszen@users.noreply.github.com>
2023-03-16 11:20:00 -0300

    Update __init__.py

--- esphome/components/voip/__init__.py
+++ esphome/components/voip/__init__.py
@@ -6,7 +6,10 @@ voip_component_ns = cg.esphome_ns.namespace('voip_component')
 VoipComponent = voip_component_ns.class_('VoipComponent', cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(VoipComponent)
+    cv.GenerateID(): cv.declare_id(VoipComponent),
+    cv.Optional(CONF_SIP_URL, default=""): cv.string_strict
+})
+    
 }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
