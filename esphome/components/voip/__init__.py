7433598feda6860b82e55442b9678b15aeedb78d
J Michalszeszen <124604121+jmichalszeszen@users.noreply.github.com>
2023-03-16 11:21:45 -0300

    Update __init__.py

--- esphome/components/voip/__init__.py
+++ esphome/components/voip/__init__.py
@@ -10,8 +10,6 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_SIP_URL, default=""): cv.string_strict
 })
     
-}).extend(cv.COMPONENT_SCHEMA)
-
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
