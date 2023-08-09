eeb6453870d683dcd6803b8a89b0d2a969daa2b5
J Michalszeszen <124604121+jmichalszeszen@users.noreply.github.com>
2023-08-09 10:14:32 -0300

    Update voip.h

--- esphome/components/voip/voip.h
+++ esphome/components/voip/voip.h
@@ -6,13 +6,24 @@
 #include "esphome/core/component.h"
 #include "esphome/core/application.h"
 #include <Udp.h>
+#include "esphome/core/log.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+
+#include "sip_client/lwip_udp_client.h"
+#include "sip_client/mbedtls_md5.h"
+#include "sip_client/sip_client.h"
+
+#include <string.h>
 // #include "abc.h"
 
-#if defined ESP8266 || defined ARDUINO_ESP8266_ESP01
-    #include <ESP8266WiFi.h>
-#else
-    #include <WiFi.h>
-#endif
+#include <WiFi.h>
 #include <WiFiUdp.h>
 
 namespace esphome {
